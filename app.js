17044abda5a3ff419c1462ec961bd94b503f68ef
mr-wang1009 <1115378579@qq.com>
2022-07-07 14:54:12 +0800

    feat：添加mysql ，静态页面

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
+const serveStatic = require('serve-static')
 
 module.exports = async function (fastify, opts) {
+  await fastify.register(require('@fastify/express'))
+  // Single path
+  fastify.use('/', serveStatic(path.join(__dirname, '/static')))
+
   // Place here your custom code!
 
   // Do not touch the following lines
@@ -15,7 +20,10 @@ module.exports = async function (fastify, opts) {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
   })
-
+  // mysql = {host, port, database, user, password}
+  const { mysql } = require('./config')
+  const mysqlOpts = Object.assign({}, mysql, opts.mysql)
+  fastify.register(require('@fastify/mysql'), mysqlOpts)
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
--- routes/example/index.js
+++ routes/example/index.js
@@ -1,7 +1,16 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
+  const mysql = fastify.mysql
   fastify.get('/', async function (request, reply) {
     return 'this is an example'
+  })
+  fastify.get('/user/:id', function (req, reply) {
+    mysql.query(
+      'SELECT id, name FROM users WHERE id=?', [req.params.id],
+      function onResult(err, result) {
+        reply.send(err || result)
+      }
+    )
   })
 }
